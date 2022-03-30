a61f3ad27c6d3f74123df83de11fc02dbc214577
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-03-30 20:35:46 +0000

    Doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,12 @@
 <?php
 
-return [
+return [    
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    VS\Next\Catalog\Infrastructure\VSNextCatalogBundle::class => ['all' => true],
+    VS\Next\Checkout\Infrastructure\VSNextCheckoutBundle::class => ['all' => true],
+    VS\Next\Shared\Infrastructure\VSNextSharedBundle::class => ['all' => true]
 ];
--- src/Catalog/Infrastructure/DependencyInjection/VSNextCatalogExtension.php
+++ src/Catalog/Infrastructure/DependencyInjection/VSNextCatalogExtension.php
@@ -4,11 +4,18 @@ namespace VS\Next\Catalog\Infrastructure\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class VSNextCatalogExtension extends Extension
+class VSNextCatalogExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('doctrine.yml');
+    }
+
     /** @param array<string, mixed> $configs */
     public function load(array $configs, ContainerBuilder $container): void
     {
