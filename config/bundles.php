419e906aabd0f43f0e83d15d10d9d5dea2723393
José Ramón Maciá Gaitán <joseramon.macia@atlasinformatica.com>
2022-03-31 07:51:45 +0000

    Routing

--- config/bundles.php
+++ config/bundles.php
@@ -6,10 +6,10 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    VS\Next\Catalog\Infrastructure\VSNextCatalogBundle::class => ['all' => true],
-    VS\Next\Checkout\Infrastructure\VSNextCheckoutBundle::class => ['all' => true],
     VS\Next\Shared\Infrastructure\VSNextSharedBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    VS\Next\Catalog\Infrastructure\VSNextCatalogBundle::class => ['all' => true],
+    VS\Next\Checkout\Infrastructure\VSNextCheckoutBundle::class => ['all' => true],
 ];
